833723980891ce8d1b859af8ee67b338bbcb9ecc
PauloAranha29 <paulo.aranha@acad.pucrs.br>
2018-11-01 20:17:39 -0300

    .

--- src/test/java/TesteMetodos.java
+++ src/test/java/TesteMetodos.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 public class TesteMetodos {
     Metodos metodos = new Metodos();
