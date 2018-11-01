2a9850a7b7edc127c523d1a45762fd4c0b6fe349
PauloAranha29 <paulo.aranha@acad.pucrs.br>
2018-11-01 20:14:41 -0300

    .

--- src/main/java/Metodos.java
+++ src/main/java/Metodos.java
@@ -1,5 +1,7 @@
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 
 public class Metodos implements InterfaceMetodos{
@@ -7,7 +9,26 @@ public class Metodos implements InterfaceMetodos{
     public Metodos(){}
 
     public ArrayList<String> fizzbuzz() {
-        return null;
+    	ArrayList<String> aux = new ArrayList<>();
+        for(int i = 1; i <= 100; i++){
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    aux.add("FizzBuzz");
+                    continue;
+                } else {
+                    aux.add("Fizz");
+                    continue;
+                }
+            }
+
+            if (i % 5 == 0) {
+                aux.add("Buzz");
+                continue;
+            }
+
+            aux.add(String.valueOf(i));
+        }
+        return aux;
     }
 
     public String calculadiferencahorario(String horario1, String horario2) {
